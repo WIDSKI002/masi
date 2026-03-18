580738df24400d67881edc0aa7dc775532ea2ede
WIDSKI002 <163059334+WIDSKI002@users.noreply.github.com>
2026-03-18 09:16:47 +0100

    init

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+  console.log(`Serwer działa na porcie ${PORT}`);
+});
